db16bd00968cd261b6d6e83d490ec72ed47399e0
John Paul Koenig <jpkoenig@umich.edu>
2020-08-05 14:04:28 -0400

    finished p1. Used loops, but there existed a closed form solution using arithmetic progression formula.

--- p1.cpp
+++ p1.cpp
@@ -4,7 +4,22 @@ using std::cout;
 
 int main() {
 
-    cout << "[output results]";
+    static const int MAXVAL = 1000;
+    int sum = 0;
 
-return 0;
+    for (int i = 1; i < MAXVAL; ++i){
+
+        // cout << "\n" << sum << "\n";
+
+        if ( i % 3 == 0 ) {
+            sum += i;
+        }
+        else {
+            sum += ( i % 5 == 0 ? i : 0);
+        }
+
+    }
+    cout << "\n" << sum << "\n";
+
+    return 0;
 }
